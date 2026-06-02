4503ae2a5eef4ee9a195f05563671994062acf4d
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-06-02 18:23:07 +0700

    hoan thien user service va them dto

--- CuoiKyWeb2/src/main/java/NguyenQuocGiakhang/CuoiKyWeb2/service/UserService.java
+++ CuoiKyWeb2/src/main/java/NguyenQuocGiakhang/CuoiKyWeb2/service/UserService.java
@@ -1,23 +1,86 @@
 package NguyenQuocGiakhang.CuoiKyWeb2.service;
 
+import java.util.List;
+
+import javax.management.relation.Role;
+
 import org.springframework.stereotype.Service;
 
 import NguyenQuocGiakhang.CuoiKyWeb2.domain.User;
+import NguyenQuocGiakhang.CuoiKyWeb2.domain.dto.RegisterDTO;
+import NguyenQuocGiakhang.CuoiKyWeb2.repository.OrderRepository;
+import NguyenQuocGiakhang.CuoiKyWeb2.repository.ProductRepository;
+import NguyenQuocGiakhang.CuoiKyWeb2.repository.RoleRepository;
 import NguyenQuocGiakhang.CuoiKyWeb2.repository.UserRepository;
 
 @Service
 public class UserService {
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
 
-	public UserService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+    public UserService(UserRepository userRepository,
+            RoleRepository roleRepository,
+            ProductRepository productRepository,
+            OrderRepository orderRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
+    }
 
-	public User getUserByEmail(String email) {
-		return this.userRepository.findByEmail(email);
-	}
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
 
-	public boolean checkEmailExist(String email) {
-		return this.userRepository.existsByEmail(email);
-	}
+    public List<User> getAllUsersByEmail(String email) {
+        return this.userRepository.findOneByEmail(email);
+    }
+
+    public User handleSaveUser(User user) {
+        User eric = this.userRepository.save(user);
+        System.out.println(eric);
+        return eric;
+    }
+
+    public User getUserById(long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public void deleteAUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    public Role getRoleByName(String name) {
+        return this.roleRepository.findByName(name);
+    }
+
+    public User registerDTOtoUser(RegisterDTO registerDTO) {
+        User user = new User();
+        user.setFullName(registerDTO.getFirstName() + " " + registerDTO.getLastName());
+        user.setEmail(registerDTO.getEmail());
+        user.setPassword(registerDTO.getPassword());
+        return user;
+    }
+
+    public boolean checkEmailExist(String email) {
+        return this.userRepository.existsByEmail(email);
+    }
+
+    public User getUserByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
+    public long countUsers() {
+        return this.userRepository.count();
+    }
+
+    public long countProducts() {
+        return this.productRepository.count();
+    }
+
+    public long countOrders() {
+        return this.orderRepository.count();
+    }
 }
