313f50b39b22ccd626f8151e3024bb1f665f9821
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-06-02 15:01:39 +0700

    test

---
+++ CuoiKyWeb2/src/main/java/NguyenQuocGiakhang/CuoiKyWeb2/CuoiKyWeb2Application.java
@@ -0,0 +1,13 @@
+package NguyenQuocGiakhang.CuoiKyWeb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuoiKyWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuoiKyWeb2Application.class, args);
+	}
+
+}
